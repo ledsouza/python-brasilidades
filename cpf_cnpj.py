b5967c94fe2dca6b111a28c3a0212f32ec14699b
Leandro Souza <leandro.souza.159@gmail.com>
2023-08-01 09:54:29 -0300

    Construindo uma Factory

---
+++ cpf_cnpj.py
@@ -0,0 +1,49 @@
+from validate_docbr import CPF, CNPJ
+
+class Documento:
+    @staticmethod
+    def criar_novo(documento):
+        doc_str = str(documento)
+        if len(doc_str) == 11:
+            return DocCpf(doc_str)
+        elif len(doc_str) == 14:
+            return DocCnpj(doc_str)
+        else:
+            raise ValueError('Documento incorreto!')
+        
+class DocCpf:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError('CPF inválido!')
+        
+    def __str__(self):
+        return self.format()
+    
+    def valida(self, documento):
+        validador = CPF()
+        return validador.validate(documento)
+    
+    def format(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+    
+class DocCnpj:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError('CNPJ inválido!')
+        
+    def __str__(self):
+        return self.format()
+    
+    def valida(self, documento):
+        validador = CNPJ()
+        return validador.validate(documento)
+    
+    def format(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cpf)
+    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from Cpf import ValidaCpf
+from cpf_cnpj import Documento
 
 cpf = 87596504000
 
-cpf_formatado = ValidaCpf(cpf)
+cpf_formatado = Documento.criar_novo(cpf)
 print(cpf_formatado)
